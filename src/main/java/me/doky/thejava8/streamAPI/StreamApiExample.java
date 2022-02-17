a7e7ffb0c54dcd597b4bdd686440aa48a6c4e83d
doky <48897815+iamdoky@users.noreply.github.com>
2022-02-17 16:06:26 +0900

    Stream API Example class

--- src/main/java/me/doky/thejava8/streamAPI/StreamApiExample.java
+++ src/main/java/me/doky/thejava8/streamAPI/StreamApiExample.java
@@ -1,7 +1,11 @@
 package me.doky.thejava8.streamAPI;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamApiExample {
 
@@ -22,28 +26,58 @@ public class StreamApiExample {
         iamdokyEvents.add(springClass);
         iamdokyEvents.add(javaClasses);
 
+        System.out.println( "spring 으로 시작하는 수업 :::::::::::::" );
+        springClass.stream().
+                filter( oc -> oc.getTitle().startsWith("spring") )
+                .forEach( oc -> System.out.println(oc.getTitle()) );
 
-        System.out.println( "spring 으로 시작하는 수업" );
-        // TODO
+        System.out.println( "close 되지 않은 수업 :::::::::::::" );
+        springClass.stream()
+                .filter( oc -> !oc.isClosed())
+                .forEach( oc -> System.out.println(oc.getTitle()));
 
-        System.out.println( "close 되지 않은 수업" );
-        // TODO
+        springClass.stream()
+                .filter( Predicate.not(OnlineClass::isClosed) )
+                .forEach( oc -> System.out.println(oc.getId()) );
 
-        System.out.println( "수업 이름만 모아서 스트림" );
-        // TODO
+        System.out.println( "수업 이름만 모아서 스트림 :::::::::::::" );
+        springClass.stream()
+                .map( oc -> oc.getTitle() )
+                .forEach( s -> System.out.println(s) );
 
+        springClass.stream()
+                .map( OnlineClass::getTitle )
+                .forEach( System.out::println );
 
-        System.out.println( "두 수업 목록에 들어있는 모든 수업 아이디 출력" );
-        // TODO
+        System.out.println( "두 수업 목록에 들어있는 모든 수업 아이디 출력 :::::::::::::" );
+        iamdokyEvents.stream()
+                .flatMap( Collection::stream)
+                .forEach(oc -> System.out.println(oc.getId()));
 
-        System.out.println( "10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 뺴고 최대 10개 까지만 " );
-        // TODO
+        System.out.println( "10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 뺴고 최대 10개 까지만 ::::::::::::: " );
+        // Stream<Integer> integerStream = Stream.iterate(10, i -> i + 1);
+        Stream.iterate( 10, i -> i + 1)
+                .skip(10)
+                .limit(10)
+                .forEach( System.out::println );
 
-        System.out.println( "자바 수업 중에 Test 가 들어 있는 수업이 있는지 확인" );
-        // TODO
+        System.out.println( "자바 수업 중에 Test 가 들어 있는 수업이 있는지 확인 :::::::::::::" );
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println(test);
 
-        System.out.println( "스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기" );
-        / TODo
+        System.out.println( "스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기 :::::::::::::" );
+        List<String> springs = springClass.stream()
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(OnlineClass::getTitle)
+                .collect(Collectors.toList());
+        springs.forEach(System.out::println);
+
+        List<String> springs2 = springClass.stream()
+                .map(OnlineClass::getTitle)
+                .filter(t -> t.contains("spring"))
+                .collect(Collectors.toList());
+        springs2.forEach(System.out::println);
+
 
     }
 
